d53ec7af469ef6712620d3f389a47c0c679b1cfb
GitHub Student <CollectionOfAtoms@gmail.com>
2014-02-25 23:01:14 -0700

    Expanded date range for new movies and added checkbox functionality

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,13 +9,44 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
     @all_ratings = ['G','PG','PG-13','R','NC-17','NR'] #Array of all possible ratings to reference to
-    @checked = {}                                      #Empty array will be filled with whatever 
+    @checked = {}                    #Empty array will be filled with checked values
+
+    if params[:ratings] != nil
+    	@all_ratings.each { |rating|
+    		@checked[rating] = params[:ratings].has_key?(rating)
+    	}
+    else
+    	@all_ratings.each { |rating|
+    		@checked[rating] = true
+    	}
+    end
 
     if(params[:sort].to_s == 'title')
     	@movies = @movies.sort_by {|mov| mov.title}
     elsif(params[:sort].to_s == 'release')
     	@movies = @movies.sort_by {|mov| mov.release_date.to_s}
     end
+
+
+    # set the checked hash to make checkboxes be checked
+=begin	@all_ratings.each { |rating|
+      if params[:ratings] == nil
+        @checked[rating] = false
+      else
+        @checked[rating] = params[:ratings].has_key?(rating)
+      end
+    }
+=end
+
+    # Sort Based on Selected Ratings
+    if(params[:ratings] != nil)
+    	@movies = @movies.find_all{ |m| params[:ratings].has_key?(m.rating) }
+    	session[:ratings] = params[:ratings]
+    elsif
+    	(session[:ratings] != nil)
+   		@movies = @movies.find_all{ |m| session[:ratings].has_key?(m.rating) }
+  	end
+
   end
 
   def new
